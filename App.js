90deb355b59db278597593712813df4f4c0eee75
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-05-22 09:27:41 -0500

    Testing React Native

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Image goes here.</Text>
+      <Image
+        style={imageStyles}
+        source={{uri: 'https://cards.scryfall.io/normal/front/4/2/42ecb371-53aa-4368-8ddd-88ae8e90ae0c.jpg'}}
+      />
     </View>
   );
 }
@@ -18,3 +21,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const imageStyles = {
+  transform: 'rotate(90deg)',
+  width: 100,
+  height: 150
+}
