e1c6ef838576d648eee0fb0e79ac3089821c2497
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-05-23 09:37:59 -0500

    Card now displays nicely

--- src/Card.js
+++ src/Card.js
@@ -1,33 +1,58 @@
+import {useState} from 'react';
 import {StyleSheet, Image, Text, View} from 'react-native';
 
-const styles = StyleSheet.create({
-    view: {
-        width: '80%'
-    },
-    figure: {
-        width: 100,
-        height: 150,
-    },
-    name: {
-        borderBottomColor: "black",
-        borderBottomWidth: 1
-    },
-    image: {
-        width: '100%',
-        height: '100%',
-        transform: 'rotate(90deg)',
-        resizeMode: 'cover'
-    }
-});
+const RATIO = 0.69841;
+const OFFSET = -2;
 
 const Card = props => {
+
+    const [visible, setVisible] = useState(true);
+    const height = Math.ceil(props.size * RATIO);
+    const width = props.size;
+
+    const styles = StyleSheet.create({
+        view: {
+            width: width,
+            height: height,
+            borderStyle: "solid",
+            borderWidth: 2,
+            borderRadius: 15,
+            borderColor: "black",
+            overflow: "hidden"
+        },
+        name: {
+            textAlign: "center"
+        },
+        type: {
+            textAlign: "center",
+            borderBottomColor: "black",
+            borderBottomWidth: 1
+        },
+        text: {
+            padding: 5
+        },
+        image: {
+            position: 'absolute',
+            left: width+OFFSET,
+            top: OFFSET,
+            transformOrigin: 'top left',
+            transform: [{rotate: '90deg'}],
+            width: height,
+            height: width,
+            display: visible? "block": "none"
+        }
+    });
+
+    const flip = () =>{
+        setVisible(!visible);
+    }
+
     return (
-        <View style={styles.view}>
+        <View style={styles.view} onClick={flip}>
             <Text style={styles.name}>{props.card.name}</Text>
-            <Text>{props.card.text}</Text>
-            <View style={styles.figure}>
-                <Image style={styles.image} source={{uri:props.card.image_uri}} />
-            </View>
+            <Text style={styles.type}>{props.card.type}</Text>
+            <Text style={styles.text}>{props.card.text}</Text>
+            <Image style={styles.image} source={{uri:props.card.image_uri}} />
         </View>
     )
 };
