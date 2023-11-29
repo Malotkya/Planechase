1255f3ea7bb76e834f2511828ae049efeb6461a2
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-11-29 10:53:53 -0600

    Init card list.

--- src/CardList.js
+++ src/CardList.js
@@ -1,19 +1,48 @@
 import {useState, useEffect} from "react";
 import {View, Text, Button} from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import list from "../cards.json";
-
-const STORAGE_KEY = "PlanechaseOptions"
+import initList from "../cards.json";
 
 const CardList = (props) => {
     const [state, setState] = useState(undefined);
+
+    const getList = () =>{
+        const list = [];
+
+        for(let attribute in state){
+            for(let card of state[attribute]){
+                if(card.use)
+                    list.push(card);
+            }
+        }
+
+        return list;
+    }
     
     useEffect(()=>{
-        props.callback(list.Plane)
-    }, [])
+        if(state === undefined){
+            AsyncStorage.getItem("state").then(value=>{
+                if(value === null) {
+                    value = {};
+                    for(let name in initList){
+                        value[name] = initList[name].map(card=>{
+                            card.use = true;
+                            return card;
+                        });
+                    }
+    
+                    setState(value);
+                }
+            });
+        } else {
+            props.callback(getList());
+        }
+    }, [state])
 
     return (
         <Text>Deck List</Text>
+
     )
 }
 
