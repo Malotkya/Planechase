9a71fd83613732c58544a7a762bbcea8ad063731
Malotkya <47186158+Malotkya@users.noreply.github.com>
2023-12-01 09:24:53 -0600

    Comments explaining what the constants are for.

--- src/Constants.js
+++ src/Constants.js
@@ -3,13 +3,19 @@
  * @author Alex Malotky
  */
 
+//Ratio to switch from Width to Height
 export const RATIO = 0.69841;
+//Ratio to switch from Height to Width
 export const INVERTSE_RATIO = 1 / RATIO;
+
+//Max Width of the App
 export const MAX_SIZE = 850;//px
 
+//Default Dimensions of a button.
 export const BUTTON_WIDTH = 100;//px
 export const BUTTON_HEIGHT = 35;//px
 
+//Default Button Styling
 export const BUTTON_DEFAULT = {
     width: BUTTON_WIDTH,
     height: BUTTON_HEIGHT,
