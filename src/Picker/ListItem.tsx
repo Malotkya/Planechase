2c139d7f89bbdd30eafc261ebf7106e244f39c60
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-04-25 14:35:37 -0500

    Renamed Card to ListItem

--- src/Picker/ListItem.tsx
+++ src/Picker/ListItem.tsx
@@ -5,27 +5,29 @@
 import {useState, useEffect} from "react";
 import {TouchableOpacity, Text, StyleSheet} from "react-native";
 import Checkbox from 'expo-checkbox';
+import { BUTTON_HEIGHT } from "../Constants";
 
 interface CardProps {
     value:CardBase
     onValueChange: (value:boolean)=>void
 }
 
-export default function Card(props:CardProps){
+export default function ListItem(props:CardProps){
     const {value = {}, onValueChange} = props;
     if(typeof onValueChange !== "function")
         throw new TypeError("onValueChange must be a function!");
-
+    
     const {name = "undefined", use = false} = value as CardBase;
-
     const [state, setState] = useState(use);
+    
 
     const styles = StyleSheet.create({
         touch: {
             padding: "3px",
             flexDirection: "row",
             borderBottomColor: "black",
-            borderBottomWidth: 1
+            borderBottomWidth: 1,
+            height: BUTTON_HEIGHT
         },
         text: {
             paddingLeft: "5px"
