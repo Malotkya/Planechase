f253099bea5f7c83a7b0a72d7890903d2e4d4ecc
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-04-26 10:46:45 -0500

    Added Wanted Card to Boutny.

--- src/Bounty.tsx
+++ src/Bounty.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {View, StyleProp, ViewStyle} from "react-native";
+import {View} from "react-native";
 
 import CardPicker from "./Picker"
 import Deck from "./Deck";
@@ -8,6 +8,13 @@ interface BountyProps {
     init: GameVersion,
     size:number
 }
+
+const WANTED = {
+    "name": "Wanted!",
+    "text": "Before the game, shuffle at least 6 unique bounty cards into a face-down pile.\nAs the starting player's third turn begins, reveal the top bounty card.\nClaim the revealed bounty during your turn and collect your reward!\nAs each turn begins, if no bounty is being offered, reveal the next one. If the pile is empty, shuffle all claimed bounties and restock\nIf the bounty went unclaimed last turn, increase its reward to the next level.\n\nRewards\n1 — Create a Treasure token\n2 — Create two Treasure tokens\n3 — Create two Treasure tokens *or* draw a card\n4 — (Max) Create two Treasure tokens *and* draw a card.",
+    "type": "Card",
+    "image_uri": "https://cards.scryfall.io/normal/back/a/c/acd27632-4c28-4dc3-90ad-b94fe176b91a.jpg?1712319002"
+  }
 
 export default function Bounty(props:BountyProps) {
     const {init, size} = props;
@@ -33,7 +40,7 @@ export default function Bounty(props:BountyProps) {
     return (
         <View>
             <CardPicker callback={shuffleCards} size={size} init={[init]} storageKey="bounty"/>
-            <Deck list={list} size={size} shuffle={()=>shuffleCards(list)}/>
+            <Deck list={list} size={size} shuffle={()=>shuffleCards(list)} additonal={WANTED}/>
         </View> 
     );
 }
