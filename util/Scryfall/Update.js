b1b83772c96a506f3c0f6eb52b1888009cfaf789
Alex M <ZimTKE@gmail.com>
2026-01-21 10:49:35 -0600

    Updated types and switch to async/await where ever possible.

--- util/Scryfall/Update.js
+++ util/Scryfall/Update.js
@@ -1,113 +1,124 @@
-const https = require('https');
+const fetch = require("./Fetch");
 const fs = require('fs');
 const ImportStream = require("./ImportStream");
 const OptimizeStream = require("./OptimizeStream");
 
-const BULK_DATA_URI = "https://api.scryfall.com/bulk-data";
-
 /** Get Oracle Database Info
  * 
- * @returns {Promise<any>}
+ * @returns {Promise<{
+ *       object: "bulk_data",
+ *           id: string,
+ *         type: "oracle_cards",
+ *   updated_at: string,
+ *          uri: string,
+ *         name: string,
+ *  description: string,
+ *         size: number,
+ * download_uri: string,
+ * content_type: string,
+ * content_encoding: string
+ * }>}
  */
-function getOracleDatabaseInfo(){
-    return new Promise((resolve, reject)=>{
+async function getOracleDatabaseInfo(){
+    const response = await fetch("/bulk-data");
+    const {data}   = await response.json();
 
-        https.get(BULK_DATA_URI, (response)=>{
-            let buffer = "";
+    for(const item of data) {
+        if(item.name === "Oracle Cards") {
+            return item;
+        }
+    }
 
-            response.on("data", data =>{
-                buffer += data.toString();
-            });
-
-            response.on("error", e =>{
-                reject(e);
-            })
-
-            response.on("close", ()=>{
-                try {
-                    let json = JSON.parse(buffer);
-                    json.data.forEach(obj=>{
-                        if(obj.type === "oracle_cards")
-                            resolve(obj);
-                    });
-
-                    reject(new Error("Unable to find Oracle Cards!"));
-                } catch(e){
-                    reject(e);
-                }
-            });
-        });
-    });
+    throw new Error("Unable to find oracle cards in response!");
 };
 
 /** Check For Update
  * 
- * @param {string} lastUpdate 
- * @returns {Promise<any|undefined>} - update infomration.
+ * @param {Date} lastUpdate 
+ * @returns {Promise<{
+ *       object: "bulk_data",
+ *           id: string,
+ *         type: "oracle_cards",
+ *   updated_at: string,
+ *          uri: string,
+ *         name: string,
+ *  description: string,
+ *         size: number,
+ * download_uri: string,
+ * content_type: string,
+ * content_encoding: string
+ * }|null>} - update infomration.
  */
-function checkForUpdate(lastUpdate){
-    return new Promise((resolve, reject)=>{
-        getOracleDatabaseInfo().then(data=>{
-            if(new Date(data.updated_at).valueOf() > new Date(lastUpdate).valueOf())
-                resolve(data);
-            else
-                resolve(undefined);
-        }).catch(reject)
-    });
+async function checkForUpdate(lastUpdate){
+    const data = await getOracleDatabaseInfo();
+
+    if(new Date(data.updated_at).valueOf() > lastUpdate.valueOf()) {
+        return data;
+    } else {
+        return null;
+    }
 }
 
 /** Get File Metadata
  * 
  * @param {string} filename 
- * @returns {string} - date information.
+ * @returns {Promise<Date>} - date information.
  */
 function getDatabaseMetadata(filename){
     return new Promise( (resolve, reject)=>{
         fs.stat(filename, (error, stats)=>{
             if(stats){
-                resolve(stats.ctime.toUTCString()); 
+                resolve(stats.ctime); 
             }
 
-            resolve(new Date(0).toUTCString());
+            resolve(new Date(0));
         });
     });
 }
 
 /** Download Update
  * 
- * @param {string} uri 
+ * @param {URL} url
  * @param {number} size
  * @returns {Promise<string>} - temp filename
  */
-function download(uri, size){
+function download(url, size){
     return new Promise((resolve, reject)=>{
         const fileName = "download.json";
-        const fileStream = fs.createWriteStream(fileName);
+
+        const close = (e) => {
+            //Delete temp file on error.
+            try {
+                fs.unlinkSync(fileName);
+            } catch (_){
+
+            }
+            reject(e);
+        }
 
         /** Update
          * 
          * @param {number} value
-         * @param {boolean} refresh
          */
-        const update = (value, refresh=true) => {
+        const update = (value) => {
             const percent = Math.round((value / size) * 1000) / 10;
             process.stdout.write(`\u001b[1AProgress: ${percent}% \n`); 
         }
 
         process.stdout.write('\n');
-        https.get(uri, response=>{
+        fetch(url).then((response)=>{
             response.pipe(new ImportStream())
                 .on("log", update)
-                .on("error", reject)
-            .pipe( new OptimizeStream())
+                .on("error", close)
+            .pipe(new OptimizeStream())
                 .on("log", console.log)
-                .on("error", reject)
-            .pipe(fileStream)
-                .on("error", reject)
+                .on("error", close)
+            .pipe(fs.createWriteStream(fileName))
+                .on("error", close)
                 .on("close", ()=>{
-                    resolve(fileName);
-                })
-        }).on("error", reject);
+                    resolve(fileName)
+                });
+        }).catch(close);
     });
 }
 
@@ -116,28 +127,23 @@ function download(uri, size){
  * @param {string} filename 
  * @returns {Promise<void>}
  */
-function update(filename){
-    getDatabaseMetadata(filename).then(date=>{
-        console.log(date);
+module.exports = async function update(filename){
+    const date = await getDatabaseMetadata(filename);
+    console.log(date.toString());
 
-        checkForUpdate(date).then(update=>{
+    const update = await checkForUpdate(date);
+    if(update === null) {
+        console.log("Everything is up to date!");
+        return;
+    }
 
-            if(update){
-                download(update.download_uri, update.size).then(newFileName=>{
-                    try {
-                        fs.unlinkSync(filename);
-                    } catch(e){
-                        //Dont care if file doesn't exist.
-                    }
-                    
-                    fs.renameSync(newFileName, filename);
-                });
+    console.log(update.download_uri);
+    const tempName = await download(new URL(update.download_uri), update.size);
+    try {
+        fs.unlinkSync(filename);
+    } catch(_){
+        //Dont care if file doesn't exist.
+    }
 
-            } else {
-                console.log("Everything is up to date!");
-            }
-        });
-    });
+    fs.renameSync(tempName, filename);
 }
-
-module.exports = update;
