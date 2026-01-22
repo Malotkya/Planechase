6f565c221358473d99268dcdf89def059946a6cf
Alex M <ZimTKE@gmail.com>
2026-01-22 10:01:11 -0600

    Fixed issue wiht cards shuffling when the about menu is shown.

--- src/Main.tsx
+++ src/Main.tsx
@@ -21,15 +21,19 @@ const Items:Array<GameVersion[]> = [
 ]
 
 export default function Main({state, dispatch}:MainProps) {
-    const [list, setList] = useState<Array<GameVersion>>(Items[state.current]);
+    const [deck, setDeck] = useState({
+        current: state.current,
+        allCards: Items[state.current],
+        selected: getSelected(Items[state.current])
+    });
 
-    /** Get Randomized List of Selected Cards
+    /** Get List of Selected Cards
      * 
      * Uses eather the state stored or state passed in.
      * 
      * @returns {Array<CardBase>}
      */
-    function getCards(cards:Array<GameVersion> = list):Array<CardBase> {
+    function getSelected(cards:GameVersion[]):Array<CardBase> {
         const output:Array<CardBase> = [];
 
         for(const version of cards){
@@ -54,35 +58,55 @@ export default function Main({state, dispatch}:MainProps) {
      * @param {string} name 
      * @param {CardList} value 
      */
-    const updateListState = (name:string, value:CardList) => {
-        const update = list.map((game)=>{
-            if(game.name === name)
-                game.value = value
-            return game
-        })
-        AsyncStorage.setItem(state.current.toString(), JSON.stringify(update));
-        setList(update);
+    const updateDeckState = (name:string, value:CardList) => {
+        setDeck(({allCards, current}:typeof deck)=>{
+            const update = allCards.map((game)=>{
+                if(game.name === name)
+                    game.value = value
+                return game
+            });
+
+            AsyncStorage.setItem(deck.current.toString(), JSON.stringify(update));
+
+            return {
+                allCards: update,
+                current,
+                selected: getSelected(update)
+            }
+        });
+    }
+
+    const loadCurrentList = () => {
+        AsyncStorage.getItem(state.current.toString()).then(value=>{
+            let list:GameVersion[];
+            if(value) {
+                list = JSON.parse(value);
+            } else {
+                list = Items[state.current];
+            }
+
+            setDeck({
+                current: state.current,
+                allCards: list,
+                selected: getSelected(list)
+            })
+        });
     }
 
     useEffect(()=>{
-        (async()=>{
-            const value = await AsyncStorage.getItem(state.current.toString());
-
-            let list:GameVersion[];
-            if(value){
-                list = JSON.parse(value);
-            } else {
-                list = Items[state.current]
-            }
-
-            setList(list)
-        })()
+        if(state.current !== deck.current) {
+            loadCurrentList()
+        }
     }, [state.current])
+
+    useEffect(()=>{
+        loadCurrentList()
+    }, [])
 
     return (
         <View>
-            <CardPicker callback={updateListState} state={state} list={list}/>
-            <Deck list={getCards()} state={state} dispatch={dispatch}/>
+            <CardPicker callback={updateDeckState} state={state} list={deck.allCards}/>
+            <Deck list={deck.selected} state={state} dispatch={dispatch}/>
         </View> 
     );
 }
