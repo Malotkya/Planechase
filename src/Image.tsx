391b9a7f3854dea90105800d5c88be31c83faaaa
Alex M <ZimTKE@gmail.com>
2026-01-23 09:35:50 -0600

    Moved the image prefetch around so that the image removes itself  from the dom when it loads or errors.

--- src/Image.tsx
+++ src/Image.tsx
@@ -4,32 +4,34 @@ const cache:Record<string, HTMLImageElement> = {};
 
 export async function prefetch(uris:string[]):Promise<void> {
     await Promise.all(uris.map(uri=>new Promise<void>(res=>{
-        const img = createImage(uri, '1px', '1px', (e)=>{
-            if(e.type === "error") {
-                console.error("Failed to preload image: " + uri);
-            }
-            res();
-        });
+        const img = createImage(uri, '1px', '1px');
         document.body.appendChild(img);
-        cache[uri] = img;
-    })))
-}
 
-function createImage(src:string, width:string = '100%', height:string = '100%', callback?:EventListener):HTMLImageElement {
-    const img = new window.Image();
-    img.src = src;
-    img.draggable = false;
-
-    if(callback) {
-        const listener = (e:Event)=>{
+        const listener = (e:Event) => {
             img.removeEventListener("error", listener);
             img.removeEventListener("load", listener);
-            callback(e);
+            
+            if(e.type === "error") {
+                console.error("There was a error when precaching: " + uri + "!");
+            } else {
+                img.style.width = '100%';
+                img.style.height = '100%';
+                cache[uri] = img;
+            }
+
+            document.body.removeChild(img);
+            res();
         }
 
         img.addEventListener("error", listener);
         img.addEventListener("load", listener);
-    }
+    })))
+}
+
+function createImage(src:string, width:string = '100%', height:string = '100%'):HTMLImageElement {
+    const img = new window.Image();
+    img.src = src;
+    img.draggable = false;
 
     //Match NativeImage Styling 
     img.setAttribute("style", 
@@ -51,17 +53,8 @@ function get(uri:string|undefined):HTMLImageElement {
     
     if( !(uri in cache) )
         cache[uri] = createImage(uri);
-    
 
-    const elm = cache[uri];
-
-    if(document.body.contains(elm)) {
-        document.body.removeChild(elm);
-        elm.style.width = "100%";
-        elm.style.height = "100%";
-    }
-
-    return elm;
+    return cache[uri];
 }
 
 export default function Image({source, ...props}:ImageProps) {
